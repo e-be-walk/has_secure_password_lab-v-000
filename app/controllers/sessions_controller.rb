0a9265b359611952307a2f36e43c11825ccf6d26
Erin Walker <erinb0317@hotmail.com>
2018-05-31 00:43:25 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       current_user(@user)
       redirect_to user_path(@user)
     else
-      redirect_to login_path
+      redirect_to user_path
     end
   end
 end
